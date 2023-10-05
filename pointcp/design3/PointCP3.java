4bde5d6a5d0753b42f6927b2cef0ad3f542e924e
aparnakagini <aparna17k@gmail.com>
2023-10-04 23:57:23 -0400

    Upload assignment files

--- pointcp/design3/PointCP3.java
+++ pointcp/design3/PointCP3.java
@@ -1,55 +1,168 @@
-public class PointCP3 {
-    private double x;
-    private double y;
+/**
+ * This class contains instances of coordinates in either polar or
+ * cartesian format.  It also provides the utilities to convert
+ * them into the other type. It is not an optimal design, it is used
+ * only to illustrate some design issues.
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge
+ * @version July 2000
+ */
+public class PointCP3
+{
+  //Instance variables ************************************************
 
-    // Constructor for Cartesian coordinates
-    public PointCP3(double x, double y) {
-        this.x = x;
-        this.y = y;
+  /**
+   * Contains C(artesian) or P(olar) to identify the type of
+   * coordinates that are being dealt with.
+   */
+  private char typeCoord;
+  
+  /**
+   * Contains the current value of X.
+   */
+  private double x;
+
+  /**
+   * Contains the current value of Y.
+   */
+  private double y;
+	
+  
+  //Constructors ******************************************************
+
+  /**
+   * Constructs a coordinate object, with a type identifier.
+   */
+  public PointCP3(char type, double x, double y)
+  {
+    if(type != 'C' && type != 'P')
+      throw new IllegalArgumentException();
+    this.x = x;
+    this.y = y;
+    typeCoord = type;
+  }
+	
+  
+  //Instance methods **************************************************
+ 
+ 
+  public double getX()
+  {
+    return x;
+  }
+  
+  public double getY()
+  {
+    return y;
+  }
+  
+  public double getRho()
+  {
+    if(typeCoord == 'C') 
+      return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    else 
+      return getRhoFromCartesian();
+  }
+  
+  public double getTheta()
+  {
+    if(typeCoord == 'C')
+      return Math.toDegrees(Math.atan2(y, x));
+    else 
+      return getThetaFromCartesian();
+  }
+  
+  private double getRhoFromCartesian()
+  {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  }
+  
+  private double getThetaFromCartesian()
+  {
+    return Math.toDegrees(Math.atan2(y, x));
+  }
+	
+  /**
+   * Converts Cartesian coordinates to Polar coordinates.
+   */
+  public void convertStorageToPolar()
+  {
+    if(typeCoord != 'P')
+    {
+      //Calculate RHO and THETA
+      double temp = getRhoFromCartesian();
+      double theta = getThetaFromCartesian();
+      x = temp;
+      y = theta;
+      
+      typeCoord = 'P';  //Change coord type identifier
     }
-
-    // Getter for x coordinate
-    public double getX() {
-        return x;
+  }
+	
+  /**
+   * Converts Polar coordinates to Cartesian coordinates.
+   */
+  public void convertStorageToCartesian()
+  {
+    if(typeCoord != 'C')
+    {
+      //Calculate X and Y
+      double temp = getX();
+      double y = getY();
+      x = temp;
+      y = y;
+   
+      typeCoord = 'C';	//Change coord type identifier
     }
+  }
 
-    // Getter for y coordinate
-    public double getY() {
-        return y;
-    }
+  /**
+   * Calculates the distance in between two points using the Pythagorean
+   * theorem  (C ^ 2 = A ^ 2 + B ^ 2). Not needed until E2.30.
+   *
+   * @param pointA The first point.
+   * @param pointB The second point.
+   * @return The distance between the two points.
+   */
+  public double getDistance(PointCP3 pointB)
+  {
+    // Obtain differences in X and Y, sign is not important as these values
+    // will be squared later.
+    double deltaX = getX() - pointB.getX();
+    double deltaY = getY() - pointB.getY();
+    
+    return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+  }
 
-    // Method to compute polar coordinates
-    public PolarPoint toPolar() {
-        double radius = Math.sqrt(x * x + y * y);
-        double theta = Math.atan2(y, x);
-        return new PolarPoint(radius, theta);
-    }
+  /**
+   * Rotates the specified point by the specified number of degrees.
+   * Not required until E2.30
+   *
+   * @param point The point to rotate
+   * @param rotation The number of degrees to rotate the point.
+   * @return The rotated image of the original point.
+   */
+  public PointCP3 rotatePoint(double rotation)
+  {
+    double radRotation = Math.toRadians(rotation);
+    double X = getX();
+    double Y = getY();
+        
+    return new PointCP3('C',
+      (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
+      (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y));
+  }
 
-    @Override
-    public String toString() {
-        return "Cartesian (" + x + ", " + y + ")";
-    }
-
-    public static void main(String[] args) {
-        PointCP3 cartesianPoint = new PointCP3(3.0, 4.0);
-        System.out.println(cartesianPoint); // Output: Cartesian (3.0, 4.0)
-
-        PolarPoint polarPoint = cartesianPoint.toPolar();
-        System.out.println(polarPoint); // Output: Polar (5.0, 0.93)
-    }
-}
-
-class PolarPoint {
-    private double radius;
-    private double theta;
-
-    public PolarPoint(double radius, double theta) {
-        this.radius = radius;
-        this.theta = theta;
-    }
-
-    @Override
-    public String toString() {
-        return "Polar (" + radius + ", " + theta + ")";
-    }
+  /**
+   * Returns information about the coordinates.
+   *
+   * @return A String containing information about the coordinates.
+   */
+  public String toString()
+  {
+    return "Stored as " + (typeCoord == 'C' 
+       ? "Cartesian  (" + getX() + "," + getY() + ")"
+       : "Polar [" + getRho() + "," + getTheta() + "]") + "\n";
+  }
 }
--- pointcp/design5/PointCP5.java
+++ pointcp/design5/PointCP5.java
@@ -1,3 +1,7 @@
-public class PointCP5 {
-    
+public abstract class PointCP5 {
+    public abstract double getX();
+    public abstract double getY();
+    public abstract double getRho();
+    public abstract double getTheta();
 }
+
